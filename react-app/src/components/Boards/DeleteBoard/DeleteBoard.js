1912cd906dfae2f5ecc1d72d44e90bb656b8a3aa
Connie Ooi <connie.ooi18@gmail.com>
2023-07-31 17:04:27 -0700

    added delete board component

--- react-app/src/components/Boards/DeleteBoard/DeleteBoard.js
+++ react-app/src/components/Boards/DeleteBoard/DeleteBoard.js
@@ -1,19 +1,39 @@
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {fetchDeleteBoardThunk} from "../../../store/boards";
 import {useModal} from "../../../context/Modal";
+import {useHistory} from "react-router-dom";
+
+import "./DeleteBoard.css";
 
 export default function DeleteBoard({board}) {
-  // const dispatch =
+  const dispatch = useDispatch();
+  const {closeModal} = useModal();
+  const history = useHistory();
+  const currentUser = useSelector((state) => state.session.user);
+
+  console.log("this is current user", currentUser);
+
+  const deleteHandler = async () => {
+    console.log("inside delete handler");
+    await dispatch(fetchDeleteBoardThunk(board.id));
+
+    closeModal();
+    history.push(`/${currentUser.username}`);
+  };
   return (
-    <div>
-      <h2>Delete this board?</h2>
-      <div>
-        The board <span>{board.name} </span>along with its pins will be removed
-        from your profile.
+    <div className="delete-b">
+      <div className="del-title">Delete this board?</div>
+      <div className="u-sure">
+        The board <span className="b-name">{board.name} </span>along with its
+        pins will be removed from your profile. Are you sure?
       </div>
-      <div>
-        <button>Cancel</button>
-        <button>Delete</button>
+      <div className="del-b-btn">
+        <button className="cancel-btn" onClick={closeModal}>
+          Cancel
+        </button>
+        <button className="del-btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
     </div>
   );
